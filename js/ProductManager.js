864985652617f58c4a28a96652e68566e3b5bc6c
nico-4164 <nicolaschaves1991@gmail.com>
2022-11-29 22:03:15 -0300

    segunda entrega

--- js/ProductManager.js
+++ js/ProductManager.js
@@ -1,39 +1,44 @@
 
+const fs = require('fs')
+const { json } = require('stream/consumers')
+
 class ProductManager{
 
-    constructor(){
-        this.products=[]
-        this.path = "../archivos/productos.txt"
+    constructor(path){
+        this.path = path
+        this.format = "utf-8"
     }
 
     camposVacios(tittle, description, price, thumbnail, code, stock){
         return ( (tittle.trim().length==0) || (description.trim().length==0) || (price.trim().length==0) || (thumbnail.trim().length==0) || (code.trim().length==0) || (stock.trim().length==0))
     }
 
-    existeCode(code){
+    async existeCode(code){
 
-        if (this.products.length == 0) {
+        let productos = await this.getProducts()
+
+        if (productos.length == 0) {
             return false;
         }   
 
-        for (let i = 0; i < this.products.length; i++) {
-            const p = this.products[i];
+        for (let i = 0; i < productos.length; i++) {
+            const p = productos[i];
             if (p.code == code) {
                 return true;
             }
         }
     }
 
-    addProduct(tittle, description, price, thumbnail, code, stock){
+    async addProduct(tittle, description, price, thumbnail, code, stock){
 
         if (this.camposVacios(tittle, description, price, thumbnail, code, stock)) {
             console.log("campos invalidos")
         }
-        if(this.existeCode(code)){
+        if(await this.existeCode(code)){
             console.log("El codigo "+code+" es invalido para el producto "+tittle)
         }
         else{
-            const id = this.getNextId();
+            const id = await this.getNextId();
             const producto = {
                 id,
                 tittle,
@@ -43,18 +48,71 @@ class ProductManager{
                 code,
                 stock
             }
-            this.products.push(producto);
+
+            this.getProducts()
+            .then(products => {
+                products.push(producto)
+                return products
+            }).then(productNew => fs.promises.writeFile(this.path, JSON.stringify(productNew)))
         }
     }
 
-    getProducts(){
-        return this.products;
+    async updateProduct(id, tittle, description, price, thumbnail, code, stock){
+
+        let productos = await this.getProducts()
+        console.log("log antes del update");
+        console.log(productos);
+
+        for (let i = 0; i < productos.length; i++) {
+            const p = productos[i];
+
+            if (p.id == id) {
+                p.tittle=tittle;
+                p.description=description;
+                p.price=price;
+                p.thumbnail=thumbnail;
+                p.code=code;
+                p.stock=stock;
+                i=productos.length;
+            } 
+            
+        }
+        console.log("log despues del update");
+        console.log(productos);
+        fs.promises.writeFile(this.path, JSON.stringify(productos))
+        
     }
 
-    getProductById(id){
+    async deleteProduct(id){
 
-        for (let i = 0; i < this.products.length; i++) {
-            const p = this.products[i];
+        let productos = await this.getProducts()
+        const filterProductos = await productos.filter((p) => p.id !== id)
+
+        console.log("log antes del delete");
+        console.log(productos);
+
+        fs.promises.writeFile(this.path, JSON.stringify(filterProductos))
+
+        console.log("log despues del delete");
+        console.log(filterProductos);
+
+    }
+
+    getProducts = async() => {
+        return fs.promises.readFile(this.path ,this.format)
+        .then(content => JSON.parse(content))
+        .catch(e => {
+            console.log('ERROR', e)
+            return []
+        })
+    }
+
+    async getProductById(id){
+
+        let productos = await this.getProducts()
+
+        for (let i = 0; i < productos.length; i++) {
+            const p = productos[i];
 
             if (p.id == id) {
                 return p;
@@ -65,11 +123,14 @@ class ProductManager{
         
     }
 
-    getNextId(){
-        const count = this.products.length;
+    async getNextId(){
+
+        let products = await this.getProducts();
+        const count = products.length;
+
 
         if (count > 0) {
-            const lastProduct = this.products[count-1]
+            const lastProduct = products[count-1]
             const id = lastProduct.id + 1;
             return id;
         }else{
@@ -78,17 +139,25 @@ class ProductManager{
     }
 }
 
-const productManager=new ProductManager();
+async function run(){
+    console.log("inicio del codigo")
+    const productManager=new ProductManager("../archivos/productos.json");
 
-productManager.addProduct("Water Cooler AM4", "refrigeracion liquida para CPU Ryzen", "20000", "http:imagen-1", "BEASVC001", "3");
-console.log(productManager.getProducts())
+    await productManager.addProduct("Water Cooler AM4", "refrigeracion liquida para CPU Ryzen", "20000", "http:imagen-1", "BEASVC001", "3");
+    console.log(await productManager.getProducts())
 
-productManager.addProduct("RTX 4080 16Gb", "Tarjeta de video Nvidia", "500.000", "http:imagen-2", "BEASVC777", "5");
-productManager.addProduct("RX 6700 12Gb", "Tarjeta de video AMD", "300.000", "http:imagen-3", "OPP123", "7");
-console.log(productManager.getProductById(3))
+    await productManager.addProduct("RTX 4080 16Gb", "Tarjeta de video Nvidia", "500.000", "http:imagen-2", "BEASVC777", "5");
+    await productManager.addProduct("RX 6700 12Gb", "Tarjeta de video AMD", "300.000", "http:imagen-3", "OPP123", "7");
+    console.log(await productManager.getProductById(3))
 
-productManager.addProduct("ASUS B550 HDV", "Tarjeta madre de asus", "22.000", "http:imagen-4", "SIBCO548", "15");
-productManager.addProduct("Fuente ASUS 1000w", "Fuente de alimentacion asus de 1000W", "65.000", "http:imagen-5", "BEASVC001", "11");
+    await productManager.addProduct("ASUS B550 HDV", "Tarjeta madre de asus", "22.000", "http:imagen-4", "SIBCO548", "15");
+    await productManager.addProduct("Fuente ASUS 1000w", "Fuente de alimentacion asus de 1000W", "65.000", "http:imagen-5", "BEASVC001", "11");
 
-console.log(productManager.getProducts())
-console.log(productManager.getProductById(1))
+    await productManager.updateProduct(1,"Fuente ASUS 500W", "Fuente de alimentacion asus de 500W", "20.000", "http:imagen-6", "BEASVC001", "17")
+
+    await productManager.deleteProduct(2)
+    console.log("final del codigo");
+    console.log(await productManager.getProducts())
+}
+
+run()
