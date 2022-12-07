6ad0698c7e236e990e0efe3f80ed0780278f733e
nico-4164 <nicolaschaves1991@gmail.com>
2022-12-07 09:56:21 -0300

    correccion de error

--- src/app.js
+++ src/app.js
@@ -15,20 +15,44 @@ app.get('/product', (req,res)=>{
     res.send("Hola a todos, pero ahora desde express")    
 })
 
-app.get('/productos', (req, res) => res.send({productos} ) )
-app.get('/productos/:limite', (req, res) => {
+//app.get('/productos', (req, res) => res.send({productos} ) )
 
-    const productLimit = req.params.limite;
+app.get('/productos', (req, res) => {
+
+    let productLimit = req.query.limite;
     console.log(productLimit);
     let list=[]
     
-    for (let i = 0; i < productLimit; i++) {
-        const element = productos[i];
-        list.push(element);
+    if (!productLimit || (productLimit >= productos.length)) {
+        console.log("enrtro en el if");
+        console.log(productos);
+        res.send({productos})
+    } else {
+        for (let i = 0; i < productLimit; i++) {
+            const element = productos[i];
+            list.push(element);
+        }
+        console.log(list);
+        res.send({productos:list})
     }
-    console.log(list);
-    res.send({productos:list})
-
 })
+
+app.get('/productos/:pid', (req, res) => {
+
+    const pid = req.params.pid;
+
+    if (!pid) {
+        console.log("enrtro en el if");
+        res.send({productos})
+    } else {
+
+        let produto = productos.find(p => p.id === parseInt(pid,10));
+        
+        if (!produto) return res.send({error:"el producto no existe"})
+
+        res.send(produto)
+    }
+})
+
 
 app.listen(8081, () => console.log("Servidor arriba en el puerto 8081"))
